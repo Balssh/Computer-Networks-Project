4e0dfe45f382d200064b27130273ae26611e3883
balssh <george.bals25@gmail.com>
2022-05-08 12:38:47 +0300

    Iterative mode completed

--- ProiectServer.c
+++ ProiectServer.c
@@ -14,16 +14,97 @@
 #define BACKLOG 10
 #define MAXDATASIZE 100
 
+void SYNC(int new_fd, char *recbuf)
+{
+    int numbytes;
+    while(1)
+        {
+            if ((numbytes=recv(new_fd, recbuf, MAXDATASIZE-1, 0)) == -1) 
+            {
+                perror("recv");
+                exit(1);
+            }
+            if(strcmp(recbuf, "SYN") == 0) break;
+            if (send(new_fd, "", MAXDATASIZE-1, 0) == -1) perror("send");
+        }
+        recbuf[numbytes] = '\0';
+        printf("Primit: %s\n", recbuf);
+
+        if (send(new_fd, "SYN-ACK", MAXDATASIZE-1, 0) == -1) perror("send");
+
+        if ((numbytes=recv(new_fd, recbuf, MAXDATASIZE-1, 0)) == -1)
+        {
+            perror("recv");
+            exit(1);
+        }
+        recbuf[numbytes] = '\0';
+        printf("Primit: %s\n", recbuf);
+}
+
+void RECEIVE(int new_fd, char *recbuf, char *sendbuf, int *count)
+{
+    int numbytes;
+    int flag = 0;
+    while(1)
+    {
+        fflush(stdout);
+        
+        if ((numbytes=recv(new_fd, recbuf, MAXDATASIZE-1, 0)) == -1) 
+        {
+            perror("recv");
+            exit(1);
+        }
+        recbuf[numbytes] = '\0';
+        printf("Primit: %s\n", recbuf);
+        if(strcmp(recbuf, "FIN") == 0) return;
+
+        if((*count) > 5) flag = rand() % 2;
+        if(flag == 1)
+        {
+            sprintf(sendbuf, "CHECK(%d)", *count);
+            if (send(new_fd, sendbuf, MAXDATASIZE - 1, 0) == -1) perror("send");
+
+            while(1)
+            {
+                if ((numbytes=recv(new_fd, recbuf, MAXDATASIZE-1, 0)) == -1) 
+                {
+                    perror("recv");
+                    exit(1);
+                }
+                if(strcmp(recbuf, "ACK") == 0) 
+                {
+                    recbuf[numbytes] = '\0';
+                    printf("Primit: %s\n", recbuf);
+                    sprintf(sendbuf, "ACK(%d)", *count);
+                    if (send(new_fd, sendbuf, MAXDATASIZE - 1, 0) == -1) perror("send");
+                    (*count)++;
+                    break;
+                }
+                else if(strcmp(recbuf, "FIN") == 0) return;
+                sprintf(sendbuf, "CHECK(%d)", *count);
+                if (send(new_fd, sendbuf, MAXDATASIZE - 1, 0) == -1) perror("send");
+            }
+        }
+        else
+        {
+            sprintf(sendbuf, "ACK(%d)", *count);
+            if (send(new_fd, sendbuf, MAXDATASIZE - 1, 0) == -1) perror("send");
+
+            (*count)++;
+        }
+    }
+}
+
 int main(void)
 {
     int sockfd, new_fd;
     struct sockaddr_in my_addr;
     struct sockaddr_in their_addr;
     int sin_size, numbytes;
-    int yes = 1, count = 0;
+    int yes = 1, *count = malloc(sizeof(int));
     char sendbuf[MAXDATASIZE];
     char recbuf[MAXDATASIZE];
-    int flag = 0;
+    (*count) = 0;
 
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) 
     {
@@ -64,76 +145,13 @@ int main(void)
         }
         printf("server: conexiune de la: %s\n", inet_ntoa(their_addr.sin_addr));
 
-        while(1)
-        {
-            if ((numbytes=recv(new_fd, recbuf, MAXDATASIZE-1, 0)) == -1) 
-            {
-                perror("recv");
-                exit(1);
-            }
-            if(strcmp(recbuf, "SYN") == 0) break;
-            if (send(new_fd, "", MAXDATASIZE-1, 0) == -1) perror("send");
-        }
-        recbuf[numbytes] = '\0';
-        printf("Primit: %s\n", recbuf);
+        SYNC(new_fd, recbuf); // SYNCING WITH CLIENT
+        RECEIVE(new_fd, recbuf, sendbuf, count); // RECEIVING MESSAGES FROM CLIENT
 
-        if (send(new_fd, "SYN-ACK", MAXDATASIZE-1, 0) == -1) perror("send");
-
-        if ((numbytes=recv(new_fd, recbuf, MAXDATASIZE-1, 0)) == -1)
-        {
-            perror("recv");
-            exit(1);
-        }
-        recbuf[numbytes] = '\0';
-        printf("Primit: %s\n", recbuf);
-
-        while(1)
-        {
-            fflush(stdout);
-            
-            if(count == 5)
-            {
-                
-                while(1)
-                {
-                    sprintf(sendbuf, "%s[%d]", "CHECK", count);
-                    if (send(new_fd, sendbuf, MAXDATASIZE - 1, 0) == -1) perror("send");
-                    if ((numbytes=recv(new_fd, recbuf, MAXDATASIZE-1, 0)) == -1) 
-                    {
-                        perror("recv");
-                        exit(1);
-                    }
-                    if(strcmp(recbuf, "ACK") == 0) 
-                    {
-                        printf("CEPLM");
-                        sprintf(sendbuf, "ACK(%d)", count);
-                        count++;
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                if ((numbytes=recv(new_fd, recbuf, MAXDATASIZE-1, 0)) == -1) 
-                {
-                    perror("recv");
-                    exit(1);
-                }
-                recbuf[numbytes] = '\0';
-                printf("Primit: %s\n", recbuf);
-                if(strcmp(recbuf, "FIN") == 0) break;
-                sprintf(sendbuf, "ACK(%d)", count);
-                if (send(new_fd, sendbuf, MAXDATASIZE - 1, 0) == -1) perror("send");
-
-                 count++;
-            }
-        }
-       
         if (send(new_fd, "ACK", MAXDATASIZE-1, 0) == -1) perror("send");
         if (send(new_fd, "FIN", MAXDATASIZE-1, 0) == -1) perror("send");
         printf("AM INCHEIAT CONEXIUNEA\n");
         close(new_fd);
     }
-
     return 0;
 }
